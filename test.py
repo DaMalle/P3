b535b55dd25c0f4dae1c04261399360a65cecef7
DaMalle <marcfriis@protonmail.com>
2023-11-13 08:57:16 +0100

    current code

---
+++ test.py
@@ -0,0 +1,14 @@
+import time
+
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+
+
+with Picamera2() as cam:
+    config = cam.create_video_configuration()
+    cam.configure(config)
+
+    encoder = H264Encoder(10000000)
+    cam.start_recording(encoder, "noir_camera_test_2.h264")
+    time.sleep(30)
+    cam.stop_recording()
