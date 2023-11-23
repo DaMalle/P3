d862998ea4376df33ca10b6b0076bee1f48f161c
TheCrimsonLegend <58586628+TheCrimsonLegend@users.noreply.github.com>
2023-11-23 14:30:51 +0100

    updated

--- CIELAB_algorithm.py
+++ CIELAB_algorithm.py
@@ -31,7 +31,7 @@ def draw_lines(image):
     return pic
 
 
-def blob_op(image, rgb):
+def blob_op(image, image_out):
     params = cv2.SimpleBlobDetector.Params()
     params.filterByArea = True
     params.minArea = 200
@@ -42,25 +42,45 @@ def blob_op(image, rgb):
     detector = cv2.SimpleBlobDetector.create(params)
     invert_image = cv2.bitwise_not(image)
     keypoints = detector.detect(invert_image)
-    im_with_keypoints = cv2.drawKeypoints(rgb, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    im_with_keypoints = cv2.drawKeypoints(image_out, keypoints, np.array([]), (0, 0, 255),
+                                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     return im_with_keypoints
+
+
+def nothing(x):
+    pass
+
+
+cv2.namedWindow("Threshold")
+cv2.createTrackbar("Threshold a", "Threshold", 122, 255, nothing)
+cv2.createTrackbar("Threshold b", "Threshold", 125, 255, nothing)
 
 
 def lab_algorithm(image):
     lab = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
     L, a, b = cv2.split(lab)
 
-    ret, picture = cv2.threshold(a, 120, 255, cv2.THRESH_BINARY)
-    pic = cv2.bitwise_not(picture)
-    lined_image = draw_lines(pic)
-    #cv2.imshow('CIELAB-Binary', lined_image)
-    cv2.imshow('CIELAB-RGB', blob_op(lined_image, image))
-    #cv2.imshow('CIELAB-Lab', blob_op(lined_image, lab))
+    a_thresh = cv2.getTrackbarPos("Threshold a", "Threshold")
+    ret, a_picture = cv2.threshold(a, a_thresh, 255, cv2.THRESH_BINARY)
+    a_pic = cv2.bitwise_not(a_picture)
+    a_lined_image = draw_lines(a_pic)
+
+    b_thresh = cv2.getTrackbarPos("Threshold b", "Threshold")
+    ret, b_picture = cv2.threshold(b, b_thresh, 255, cv2.THRESH_BINARY)
+    b_pic = b_picture
+    b_lined_image = draw_lines(b_pic)
+    # cv2.imshow('CIELAB-Binary', a_lined_image)
+    # cv2.imshow('CIELAB-RGB', blob_op(a_lined_image, image))
+    # cv2.imshow('CIELAB-Lab - a', blob_op(a_lined_image, a_lined_image))
+    # cv2.imshow('CIELAB-Lab - b', blob_op(b_lined_image, b_lined_image))
+    img = cv2.bitwise_and(a_lined_image, b_lined_image)
+    cv2.imshow('CIELAB-Lab - bitwised', blob_op(img, image))
 
 
 while i < t_frames:
     picture = os.listdir('./Frames')[i]
     img = ('Frames/' + str(picture))
+    #img = ('Frames/' + 'Frame370.png')
     pictures.append(img)
 
     curr_img = cv2.imread(img)
